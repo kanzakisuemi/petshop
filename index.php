1f1c559dac0d950d2661df9b8e864f883a4e1f54
kanzakisuemi <jsuemikanzaki@gmail.com>
2023-01-16 20:46:35 -0300

    dogs index beta

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php include 'inc/header.php' ?>
-
+<?php $result = mysqli_query($conn, "SELECT * FROM dogs") ?>
+<table class="table caption-top">
+  <thead>
+    <tr>
+      <th scope="col">Name</th>
+      <th scope="col">Breed</th>
+      <th scope="col">Birthday</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php while($row = mysqli_fetch_array($result)): >
+      <tr>
+        <td><?= $row['dog_name'] ?></td>
+        <td><?= $row['dog_breed'] ?></td>
+        <td><?= $row['dog_birthday'] ?></td>
+      </tr?>
+    <?php endwhile ?>
+  </tbody>
+</table>
 <?php include 'inc/footer.php' ?>
