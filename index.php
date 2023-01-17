b9b1e8016fc5457a624e5c070856d167014348f2
kanzakisuemi <jsuemikanzaki@gmail.com>
2023-01-17 17:26:10 -0300

    fixing index

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php include 'inc/header.php' ?>
-<?php $result = mysqli_query($conn, "SELECT * FROM dogs") ?>
-<table class="table caption-top">
+<?php $dogs = mysqli_query($conn, "SELECT * FROM dogs") ?>
+<table class="table caption-top" style="width: 60%; margin: auto;">
   <thead>
     <tr>
       <th scope="col">Name</th>
@@ -9,11 +9,11 @@
     </tr>
   </thead>
   <tbody>
-    <?php while($row = mysqli_fetch_array($result)): ?>
+    <?php while($dog = mysqli_fetch_array($dogs)): ?>
       <tr>
-        <td><?= $row['nome'] ?></td>
-        <td><?= $row['raca'] ?></td>
-        <td><?= $row['data_nascimento'] ?></td>
+        <td><?= $dog['nome'] ?></td>
+        <td><?= $dog['raca'] ?></td>
+        <td><?= $dog['data_nascimento'] ?></td>
       </tr?>
     <?php endwhile ?>
   </tbody>
