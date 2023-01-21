cd3dcd96b609cee561cf96ca215df353d0723af4
kanzakisuemi <jsuemikanzaki@gmail.com>
2023-01-21 16:46:10 -0300

    removing placeholder

--- dogs_new.php
+++ dogs_new.php
@@ -3,9 +3,9 @@
 <body>
   <form style="margin: auto; margin-top: 3%; width: 60%;" action="dogs_create.php" method="post">
     <label for="nome" class="form-label">Name</label><br>
-    <input type="text" class="form-control" name="nome" placeholder="nome" required><br>
+    <input type="text" class="form-control" name="nome" required><br>
     <label for="raca" class="form-label">Breed</label><br>
-    <input type="text" class="form-control" name="raca" placeholder="raça" required><br>
+    <input type="text" class="form-control" name="raca" required><br>
     <label for="data_nascimento" class="form-label">Birthday</label><br>
     <input type="date" class="form-control" name="data_nascimento" required><br>
     <button class="btn btn-info" type="submit">submit</button>
