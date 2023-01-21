1e879c754ddc7fd21eff05883c4b86242735eac9
kanzakisuemi <jsuemikanzaki@gmail.com>
2023-01-21 17:32:08 -0300

    implementing akira's sugestions

--- dogs_create.php
+++ dogs_create.php
@@ -1,15 +1,15 @@
-<?php header('location:alert.php'); ?>
-<php? include 'inc/open_dbconn.php'; ?>
+<?php header('location:alert.php');
+  include 'inc/open_dbconn.php'; ?>
 <?php
-$sql = "INSERT INTO dogs (nome, raca, data_nascimento) VALUES (?, ?, ?)";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("sss", $nome, $raca, $data_nascimento);
+  $sql = "INSERT INTO dogs (nome, raca, data_nascimento) VALUES (?, ?, ?)";
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param("sss", $nome, $raca, $data_nascimento);
 
-$nome =$_POST['nome'];
-$raca =$_POST['raca'];
-$data_nascimento =$_POST['data_nascimento'];
+  $nome =$_POST['nome'];
+  $raca =$_POST['raca'];
+  $data_nascimento =$_POST['data_nascimento'];
 
-$stmt->execute();
-$stmt->close()
+  $stmt->execute();
+  $stmt->close()
 ?>
 <?php include 'inc/close_dbconn.php'; ?>
--- dogs_new.php
+++ dogs_new.php
@@ -1,6 +1,4 @@
 <?php include 'inc/header.php'; ?>
-</head>
-<body>
   <form style="margin: auto; margin-top: 3%; width: 60%;" action="dogs_create.php" method="post">
     <label for="nome" class="form-label">Name</label><br>
     <input type="text" class="form-control" name="nome" required><br>
@@ -8,6 +6,6 @@
     <input type="text" class="form-control" name="raca" required><br>
     <label for="data_nascimento" class="form-label">Birthday</label><br>
     <input type="date" class="form-control" name="data_nascimento" required><br>
-    <button class="btn btn-info" type="submit">submit</button>
+    <button class="btn btn-info" type="submit">Submit</button>
 <?php include 'inc/footer.php'; ?>
 
