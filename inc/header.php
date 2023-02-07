f49f9d7011a981c38a1365413d998293a39b31ba
kanzakisuemi <jsuemikanzaki@gmail.com>
2023-02-07 09:43:24 -0300

    link style

--- inc/header.php
+++ inc/header.php
@@ -4,10 +4,10 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Juju's Petshop</title>
-	<link href="/petshop/dist/app.css" type="text/css">
+	<link href="/petshop/dist/app.css" rel="stylesheet" type="text/css">
 	<script src="/petshop/dist/app.js"></script>
 	<?php include 'open_dbconn.php'; ?>
+	<?php include 'navbar.php'; ?>
 </head>
 <body>
-	<?php include 'navbar.php'; ?>
-	<div class="container">
+		<div class="container">
